9295c57923865db8682648b2629288dd9e804d39
ignacio.h <ignaciojhergomez@gmail.com>
2026-04-08 11:02:20 +0200

    Creado servicio para obtener un usuario hardcodeado por http en vez de hardcodear los datos del usuario entero. Pero con un bug por detección de cambios.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserCard } from './user-card/user-card';
 import { GithubUser } from './models/github.models';
+import { GithubService } from './github';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,14 @@ import { GithubUser } from './models/github.models';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  // demo con datos hardcodeados
-  testUser: GithubUser = {
-    login: 'torvalds',
-    name: 'Linus Torvalds',
-    avatar_url: 'https://avatars.githubusercontent.com/u/1024025',
-    public_repos: 8,
-    followers: 230000,
-    following: 0,
-    bio: 'Nothing to see here, move along.',
-    html_url: 'https://github.com/torvalds'
+export class App implements OnInit {
+  private githubService = inject(GithubService);
+  // Angular Zoneless no sabe cuándo cambia => bugs
+  user: GithubUser | null = null;
+
+  ngOnInit(): void {
+    this.githubService.getUser('torvalds').subscribe(data => {
+      this.user = data;
+    })
   }
 }
